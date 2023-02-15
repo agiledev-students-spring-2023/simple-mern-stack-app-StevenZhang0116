e378017d7ef45cdf87daabe26b7e46e76de5af11
StevenZhang0116 <zz2589@nyu.edu>
2023-02-14 22:12:25 -0500

    checkcheck

---
+++ front-end/src/About.js
@@ -0,0 +1,17 @@
+import mephoto from './mephoto.jpg'
+
+const About = () => {
+    return (
+        <>
+            <img src = {mephoto} width="450"></img>
+            <p>
+                Welcome aboard! This is Zihan Zhang (Steven), a senior undergraduate from the Courant Institute of Mathematical Sciences (CIMS), NYU. My primary academic interest is mechanistic biology (biophysics) and computational neuroscience, and my research in that field is supervised by Professor David W. McLaughlin (NYU). Before that, I worked with Professor Leif Ristroph (NYU) in Applied Mathematics Lab (AML) on fluid mechanics & geomorphology and with Professor Russel E. Caflisch (NYU) on electrokinetics. These experiences in turn supplement and develop my understanding of biophysics.  
+            </p>
+            <p>
+            Here are my CV and GitHub.  
+            </p>
+        </>
+    )
+}
+
+export default About
